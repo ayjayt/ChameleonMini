ed39bcdecb4cad0dc1d0dc6e33767f43f3b01369
Andrew Pikul <ajp@tplot.io>
2017-11-07 13:54:53 -0500

    Modified so UART works with XMODEM
    XMODEM doesn't use \r to indicate commands, so I added 0x01 and 0x04 to mark that buffer should
    be processed

--- Firmware/Chameleon-Mini/Terminal/Terminal.c
+++ Firmware/Chameleon-Mini/Terminal/Terminal.c
@@ -3,13 +3,14 @@
 #include "../System.h"
 #include "../LEDHook.h"
 
-
+// ptc is a layer to handle uart output
 void ptc(char c){
 	do{
 			/* Wait until it is possible to put data into TX data register.*/
 	} while(!( (USARTD1.STATUS & USART_DREIF_bm) != 0));
 	USARTD1.DATA = c;
 }
+// pts is a loop of pts
 void pts(const char * str, uint16_t len){
 	uint8_t i = 0;
 	if (len){
@@ -22,12 +23,12 @@ void pts(const char * str, uint16_t len){
 		} ptc(0);
 	}
 }
-
+// ftc is a layer to handle uart input
 volatile int16_t gtc(){
 	volatile int16_t recv;
 	if (USARTD1.STATUS & USART_RXCIF_bm) {
 		recv = USARTD1.DATA;
-		ptc((char)recv);
+		//ptc((char)recv); // get rid of this get rid of this get rid of this
 		return (int16_t) recv;
 	} else {
 		return -1;
@@ -40,11 +41,11 @@ volatile int16_t gtc(){
 uint8_t TerminalBuffer[TERMINAL_BUFFER_SIZE];
 TerminalStateEnum TerminalState = TERMINAL_UNINITIALIZED;
 
-
+// TerminalSendString was part of Terminmal.c for USB so I left it
 void TerminalSendString(const char* s) {
 	pts(s, 0);
 }
-
+// TerminalSendStringp is a '\=' send string
 void TerminalSendStringP(const char* s) {
     char c;
 
@@ -65,29 +66,31 @@ void TerminalSendHex(void* Buffer, uint16_t ByteCount)
 
 */
 
-
-char bigBuffer[64];
+// A circular buffer
+char bigBuffer[142];
 uint16_t writeC = 0, readC = 0, mark = 0;
+// returns values from bigBuffer if a \r was found
 int16_t getByteFromBuffer(void){
-	if (readC == 64){
+	if (readC == 142){
 		readC = 0;
 	}
 	if (mark){
-		if (bigBuffer[readC] == '\r') {
+		if ((bigBuffer[readC] == '\r')||(bigBuffer[readC] == 0x01)||(bigBuffer[readC] == 0x04)) {
 			mark--;
 		}
 		return bigBuffer[readC++];
 	}
 	return -1;
 }
+// writes to the circular buffer
 void newUART(void){
 	volatile int16_t rcvLocal = gtc();
 	if (rcvLocal != -1){
 		bigBuffer[writeC++] = (char)rcvLocal;
-		if (writeC == 64) {
+		if (writeC == 142) {
 			writeC = 0;
 		}
-		if ((char)rcvLocal == '\r') {
+		if (((char)rcvLocal == '\r') || ((char)rcvLocal == 0x01) || ((char)rcvLocal == 0x04)) {
 			mark++;
 		}
 	}
@@ -106,7 +109,7 @@ static void ProcessByte(void) {
         /* Byte received */
     	LEDHook(LED_TERMINAL_RXTX, LED_PULSE);
 
-        if (XModemProcessByte(Byte)) {
+        if (XModemProcessByte(Byte)) { // check xmodem
             /* XModem handled the byte */
         } else if (CommandLineProcessByte(Byte)) {
             /* CommandLine handled the byte */
